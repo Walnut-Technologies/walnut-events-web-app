b99db0fd7848925e0293446167ed8b909ae409b3
Vladi Romo <vladerkg@gmail.com>
2025-03-25 21:42:17 -0600

    sidebar header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,21 @@ import { authGuard } from './core/guards/auth.guard';
 
 import { LoginComponent } from './views/login/login.component';
 import { HomeComponent } from './views/home/home.component';
-
+import { LayoutComponent } from './layout/layout.component';
 export const routes: Routes = [
-  // { path: 'login', component: LoginComponent },
-  // { path: 'dashboard', component: HomeComponent, canActivate: [authGuard] },
-  // { path: '**', redirectTo: 'login' }
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: 'home', component: HomeComponent },
+      // { path: 'clientes', component: ClientesComponent },
+      // { path: 'eventos', component: EventosComponent },
+      // { path: 'mensajes', component: MensajesComponent },
+      // { path: 'invitaciones', component: InvitacionesComponent },
+      // { path: 'lugares', component: LugaresComponent },
+      { path: '', redirectTo: '/home', pathMatch: 'full' }
+    ]
+  }
 ];
 
 @NgModule({
