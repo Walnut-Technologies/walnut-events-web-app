3bbb332d12a5b04a611ebf18949852b56d05b204
Vladi Romo <vladerkg@gmail.com>
2025-06-09 22:40:18 -0600

    add agencies module

---
+++ src/app/modules/agencies/services/agencies.service.ts
@@ -0,0 +1,37 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environmentURL } from '../../../environments/environment';
+import { Response } from '../../../interfaces/Response';
+import { firstValueFrom } from "rxjs";
+import { Agency } from '../interfaces/agency.interface';
+@Injectable({
+  providedIn: 'root'
+})
+export class AgenciesService {
+
+  private httpHeaders = new HttpHeaders({
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+    });
+  
+    constructor(private http: HttpClient) { }
+  
+    async getAgencies(): Promise<any> {
+      try {
+        const response = await firstValueFrom(
+          this.http.get<Response<Agency[]>>(`${environmentURL._URL_AGENCIES}`,
+            { headers: this.httpHeaders, withCredentials: false }
+          )
+        );
+  
+        if (response) {
+          return response || [];
+        } else {
+          return [];
+        }
+      } catch (error) {
+        console.error('Error fetching agencies:', error);
+        return [];
+      }
+    }
+}
